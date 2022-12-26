35caa835af9b201db94aaf1a7e7dd1ad6945b9bc
akgunsalih <akgunslh@gmail.com>
2022-12-27 02:33:35 +0300

    a
    a

---
+++ acil_destek/Mobile app/uzman app/provider_flutter/uzman_acil/lib/utils/configs.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+const APP_NAME = 'Acil Destek Uzman';
+const DEFAULT_LANGUAGE = 'tr';
+
+const primaryColor = Color(0xFFD5761E);
+//var appFontFamily = GoogleFonts.workSans().fontFamily;
+
+const DOMAIN_URL = 'https://acil.fastturtles.net';
+const BASE_URL = "$DOMAIN_URL/api/";
+
+const ONESIGNAL_APP_ID = 'bbf4191a-a660-4153-97a7-55521e05cadc';
+const ONESIGNAL_REST_KEY = "Zjg1MWY3NmItNjFjYy00ZWJlLWI5MDEtZmY2NDA4NzkwYmZj";
+const ONESIGNAL_CHANNEL_ID = "ca2cb856-64ad-4d43-b72b-9f8c78db2d1a";
+
+const TERMS_CONDITION_URL = 'https://fastturtles.net/';
+const PRIVACY_POLICY_URL = 'https://fastturtles.net/';
+const HELP_SUPPORT_URL = 'https://fastturtles.net/';
+const PURCHASE_URL = 'https://fastturtles.net/';
+const GOOGLE_MAPS_API_KEY = 'AIzaSyCHJwjZjGSOBc18-3mJM8tCqDYoV3Nk9tQ';
+
+const STRIPE_MERCHANT_COUNTRY_CODE = 'TR';
+
+DateTime todayDate = DateTime(2022, 8, 24);
